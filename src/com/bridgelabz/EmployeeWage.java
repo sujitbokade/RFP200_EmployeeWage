890a06e8acdc6fd724eb5a43b7d29ac6c4628e52
sujit <sujitbokade2014@gmail.com>
2022-09-10 10:51:06 +0530

    [Sujit]Calculate MonthlyWages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,22 +9,29 @@ public class EmployeeWage {
         int wagePerHr = 20;
         int fullTimeHr = 16;
         int partTimeHr = 8;
-        double check = Math.floor(Math.random() * 10) % 3;
-        int dailyWage = 0;
-        switch ((int) check) {
-            case fullTime:
-                System.out.println("Employee is Present Full Time");
-                dailyWage = fullTimeHr * wagePerHr;
-                break;
-            case partTime:
-                System.out.println("Employee is Present Part Time");
-                dailyWage = partTimeHr * wagePerHr;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                break;
-        }
+        int monthlyWages = 0;
+        for (int day = 1; day <= 20; day++) {
+            double check = Math.floor(Math.random() * 10) % 3;
+            int dailyWage = 0;
+            switch ((int) check) {
+                case fullTime:
+                    System.out.println("Employee is Present Full Time");
+                    dailyWage = fullTimeHr * wagePerHr;
+                    break;
+                case partTime:
+                    System.out.println("Employee is Present Part Time");
+                    dailyWage = partTimeHr * wagePerHr;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
 
-        System.out.println("DailyWage is :" + dailyWage);
+            System.out.println("Daily Wage for Day=" + day + " is=" + dailyWage);
+            monthlyWages += dailyWage;
+            System.out.println();
+        }
+        System.out.println("Monthly Wages are: " + monthlyWages);
+
     }
 }
