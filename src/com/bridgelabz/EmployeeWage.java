61b30b479d96f422b1f5b2f8c862c7670d17e551
sujit <sujitbokade2014@gmail.com>
2022-09-10 10:35:16 +0530

    [Sujit]Using Switch Case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,22 +4,27 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage Program!");
-        int fullTime = 1;
-        int partTime = 2;
+        final int fullTime = 1;
+        final int partTime = 2;
         int wagePerHr = 20;
         int fullTimeHr = 16;
         int partTimeHr = 8;
         double check = Math.floor(Math.random() * 10) % 3;
         int dailyWage = 0;
-        if (check == fullTime) {
-            System.out.println("Employee is Present Full Time");
-            dailyWage = fullTimeHr * wagePerHr;
-        } else if (check == partTime) {
-            System.out.println("Employee is Present Part Time");
-            dailyWage = partTimeHr * wagePerHr;
-        } else {
-            System.out.println("Employee is Absent");
+        switch ((int) check) {
+            case fullTime:
+                System.out.println("Employee is Present Full Time");
+                dailyWage = fullTimeHr * wagePerHr;
+                break;
+            case partTime:
+                System.out.println("Employee is Present Part Time");
+                dailyWage = partTimeHr * wagePerHr;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
+
         System.out.println("DailyWage is :" + dailyWage);
     }
 }
