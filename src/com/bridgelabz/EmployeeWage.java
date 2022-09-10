13019953e4edb7bcf86d31c7cecc3faa04edb0b8
sujit <sujitbokade2014@gmail.com>
2022-09-10 10:20:21 +0530

    [Sujit]Added Part Time Employee

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,17 +4,22 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage Program!");
-        int present = 1;
+        int fullTime = 1;
+        int partTime = 2;
         int wagePerHr = 20;
-        int fullDayHr = 8;
-        double check = Math.floor(Math.random() * 10) % 2;
+        int fullTimeHr = 16;
+        int partTimeHr = 8;
+        double check = Math.floor(Math.random() * 10) % 3;
         int dailyWage = 0;
-        if (check == present) {
-            System.out.println("Employee is Present");
-             dailyWage = fullDayHr * wagePerHr;
+        if (check == fullTime) {
+            System.out.println("Employee is Present Full Time");
+            dailyWage = fullTimeHr * wagePerHr;
+        } else if (check == partTime) {
+            System.out.println("Employee is Present Part Time");
+            dailyWage = partTimeHr * wagePerHr;
         } else {
             System.out.println("Employee is Absent");
         }
-        System.out.println("DailyWage is :"+dailyWage);
+        System.out.println("DailyWage is :" + dailyWage);
     }
 }
